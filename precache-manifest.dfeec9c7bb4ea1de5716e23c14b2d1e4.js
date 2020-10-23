6198b20bdfd5703d0f24c6c6859b24523c5d1dae
Thao Phung <ttp7428@truman.edu>
2020-10-23 15:07:41 -0500

    Updates

---
+++ precache-manifest.dfeec9c7bb4ea1de5716e23c14b2d1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b751ba89df74a0bc3ee79bb72befd6",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "c73e0ae69b114b4ac7c8",
+    "url": "/monsters-rolodex-complete/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73e0ae69b114b4ac7c8",
+    "url": "/monsters-rolodex-complete/static/js/main.0e07c45c.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
